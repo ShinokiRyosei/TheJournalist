59361c2e46dbfafc94eb79cdd245d257e33fcacd
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-20 01:56:22 +0900

    [Create] ProfileImgaView

---
+++ TheJournalist/ProfileImageView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileImageView.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/20.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ProfileImageView: UIImageView {
+
+
+}
