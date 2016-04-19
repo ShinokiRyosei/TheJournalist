daaebd748e735ee9d2865b75e3a51d834ed4d78f
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-20 02:36:49 +0900

    [Create] Facebook Button and Twitter Button [Update] CommentViewController autolayout

---
+++ TheJournalist/CommentFbButton.swift
@@ -0,0 +1,21 @@
+//
+//  CommentFbButton.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/20.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class CommentFbButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
