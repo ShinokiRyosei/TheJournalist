8e377aac70fdd7ff08ba61b576724ebbf09cae25
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-19 21:54:42 +0900

    [Add] HomeCell and HomeTopicTitleLabel

---
+++ TheJournalist/HomeTopicTitleLabel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeTopicTitleLabel.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/19.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class HomeTopicTitleLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
