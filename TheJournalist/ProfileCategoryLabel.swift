bd63e2c256df5d88c9c366d861a4fd2027e488c9
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-21 13:49:57 +0900

    [Create] ProfileCategoryLabel

---
+++ TheJournalist/ProfileCategoryLabel.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileCategoryLabel.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/21.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCategoryLabel: UILabel {
+
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    
+    func updateLayout() {
+        
+    }
+
+}
