bc8361fd30e91abc8e73b11ad616886aa6ea593f
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-22 18:01:04 +0900

    [Create] Login and Signup ViewController

---
+++ TheJournalist/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/22.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ TheJournalist/SignupViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignupViewController.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/22.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
