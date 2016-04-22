cc82d7315be7f592eb2960a2300142ce7204936a
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-23 03:18:26 +0900

    [Create] TopicCell

---
+++ TheJournalist/TopicCell.swift
@@ -0,0 +1,24 @@
+//
+//  TopicCell.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/23.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class TopicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
