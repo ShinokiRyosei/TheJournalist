4de423b1665227a2c93699dd2575abbc5d3677b1
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-23 00:45:29 +0900

    [Create] Select Button (Login and Signup)

---
+++ TheJournalist/UnselectedLoginButton.swift
@@ -0,0 +1,21 @@
+//
+//  UnselectedLoginButton.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/23.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class UnselectedLoginButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
