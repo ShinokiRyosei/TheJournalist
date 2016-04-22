7a7432d091296994ff290b5bc0dace6a0dfca0a9
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-23 03:21:55 +0900

    [Update] topicTable (UITableView)

--- TheJournalist/TopicViewController.swift
+++ TheJournalist/TopicViewController.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class TopicViewController: NavigationViewController {
+class TopicViewController: NavigationViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var topicTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        topicTable.registerNib(UINib(nibName: "TopicCell",bundle: nil), forCellReuseIdentifier: "TopicCell")
+        
+        topicTable.delegate = self
+        topicTable.dataSource = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        topicTable.estimatedRowHeight = 120
+        topicTable.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,5 +35,15 @@ class TopicViewController: NavigationViewController {
     
     func transition() {
         self.performSegueWithIdentifier("toVoteView", sender: self)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = topicTable.dequeueReusableCellWithIdentifier("TopicCell", forIndexPath: indexPath) as! TopicCell
+        
+        return cell
     }
 }
