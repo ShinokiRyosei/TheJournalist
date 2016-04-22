2b6b6fcd60e1f24898cd2a79004b193b2cc0f341
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-23 01:30:06 +0900

    [Fix] login view

--- TheJournalist/LoginViewController.swift
+++ TheJournalist/LoginViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet var containerView: UIView!
+    //ログインを選ぶボタン tag 1
+    @IBOutlet var selectLoginButton: UIButton!
+    //新規登録を選ぶボタン tag 2
+    @IBOutlet var selectSignupButton: UIButton!
     
     var loginNib: LoginFormView!
     
@@ -21,7 +25,7 @@ class LoginViewController: UIViewController {
         self.loginNib = UINib(nibName: "LoginFormView", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! LoginFormView
         loginNib.translatesAutoresizingMaskIntoConstraints = false
         signupNib = UINib(nibName: "SignupFormView", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! SignupFormView
-        signupNib.translatesAutoresizingMaskIntoConstraints = false
+        
         addSubviewWithAutoLayout(loginNib, parentView: containerView)
         
     }
@@ -33,12 +37,15 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func didSelectLogin() {
+    @IBAction func didSelectLogin(sender: UIButton) {
         addSubviewWithAutoLayout(loginNib, parentView: containerView)
+        self.switchSelectedButton(sender)
     }
     
-    @IBAction func didSelectSignup() {
+    @IBAction func didSelectSignup(sender: UIButton) {
+        signupNib.translatesAutoresizingMaskIntoConstraints = false
         addSubviewWithAutoLayout(signupNib, parentView: containerView)
+        switchSelectedButton(sender)
     }
     
     private func addSubviewWithAutoLayout(childView: UIView, parentView: UIView) {
@@ -50,5 +57,16 @@ class LoginViewController: UIViewController {
         parentView.addConstraint(NSLayoutConstraint(item: childView, attribute: .Bottom, relatedBy: .Equal, toItem: parentView, attribute: .Bottom, multiplier: 1.0, constant: 0))
      
         parentView.addConstraint(NSLayoutConstraint(item: childView, attribute: .Left, relatedBy: .Equal, toItem: parentView, attribute: .Left, multiplier: 1.0, constant: 0))
+    }
+    
+    private func switchSelectedButton(button: UIButton) {
+        if button.tag == 1 {
+            selectLoginButton.setImage(UIImage(named: "selectedLoginButton.png"), forState: .Normal)
+            selectSignupButton.setImage(UIImage(named: "unselectedSignupButton.png"), forState: .Normal)
+        }else if button.tag == 2 {
+            selectLoginButton.setImage(UIImage(named: "unselectedLoginButton.png"), forState: .Normal)
+            selectSignupButton.setImage(UIImage(named: "selectedSignupButton.png"), forState: .Normal)
+        }
+        
     }
 }
