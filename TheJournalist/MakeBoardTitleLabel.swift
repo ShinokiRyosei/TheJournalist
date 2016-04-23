c035ec90401ed071a7c47838657638bc564db0d9
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-23 18:01:33 +0900

    [Update] MakeBoardTitleLabel

--- TheJournalist/MakeBoardTitleLabel.swift
+++ TheJournalist/MakeBoardTitleLabel.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class MakeBoardTitleLabel: UILabel {
     
-    
+    let padding = UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)
 
     override func drawRect(rect: CGRect) {
-        // Drawing code
+        let newRect = UIEdgeInsetsInsetRect(rect, padding)
+        super.drawRect(newRect)
     }
     
     override func intrinsicContentSize() -> CGSize {
-        
+        var intrinsicContentSize = super.intrinsicContentSize()
+        intrinsicContentSize.height += padding.top + padding.bottom
+        intrinsicContentSize.width += padding.left + padding.right
+        return intrinsicContentSize
     }
-
 }
