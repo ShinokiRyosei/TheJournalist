8d9914175d0562e5129870ff51da72c048db6120
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-23 17:45:49 +0900

    [Create] MakeBoardView

---
+++ TheJournalist/MakeBoardViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MakeBoardViewController.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/23.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class MakeBoardViewController: NavigationViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
