fe914d02cda9a1e4febd9a7718a8ebc671463d04
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-23 17:56:45 +0900

    [Create] MakeBoardTitleLabel

---
+++ TheJournalist/MakeBoardTitleLabel.swift
@@ -0,0 +1,23 @@
+//
+//  MakeBoardTitleLabel.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/23.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class MakeBoardTitleLabel: UILabel {
+    
+    
+
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        
+    }
+
+}
