f0f5050284c8ef612351a4b2bbeb3f572615280a
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-04-24 19:43:47 +0900

    [Create] CommentFormView

---
+++ TheJournalist/CommentFormViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CommentFormViewController.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/04/24.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class CommentFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
