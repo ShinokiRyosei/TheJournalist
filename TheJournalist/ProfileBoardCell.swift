fc9068473780148545ed5c46cd7f0749f6f7b69d
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-05-01 18:42:51 +0900

    [Create] Profile Board Clip Point Cell

---
+++ TheJournalist/ProfileBoardCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileBoardCell.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/05/01.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ProfileBoardCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TheJournalist/ProfileClipCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileClipCell.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/05/01.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ProfileClipCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
