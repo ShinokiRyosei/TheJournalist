3b39bdd23a94e80483a87a5e4d35448c6b0f36d0
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-05-01 17:02:20 +0900

    [Create] Notification Vote Comment Clip Cell

---
+++ TheJournalist/NotificationVoteCell.swift
@@ -0,0 +1,32 @@
+//
+//  NotificationVoteCell.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/05/01.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class NotificationVoteCell: UITableViewCell {
+    
+    @IBOutlet var boardTitleLabel: UILabel!
+    
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBOutlet var profileImageView: UIImageView!
+    
+    @IBOutlet var numberOfUserLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
