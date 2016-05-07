e7c8e51a94db027945202ac76c547e5bd40539c5
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-05-08 01:09:22 +0900

    [Create] Board Agree and Disagree Cell

---
+++ TheJournalist/BoardAgreeCell.swift
@@ -0,0 +1,24 @@
+//
+//  BoardAgreeCell.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/05/08.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class BoardAgreeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
