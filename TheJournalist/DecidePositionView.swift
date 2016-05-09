c730cd21d6fc4d3f7075d64080c7fcdb22671048
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-05-09 22:17:18 +0900

    [Create] CommentXIB (CommentFormView and DecidePositionView)

---
+++ TheJournalist/DecidePositionView.swift
@@ -0,0 +1,21 @@
+//
+//  DecidePositionView.swift
+//  TheJournalist
+//
+//  Created by ShinokiRyosei on 2016/05/09.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class DecidePositionView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
